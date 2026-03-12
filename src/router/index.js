51487f1efbb940c4be9aaa884b5f3c917df7af6d
Franklin <franklinmburuu05@gmail.com>
2026-03-12 13:33:14 -0800

    Revert "the rest of the components with composition api"
    This reverts commit 2f75f11db6dcc806f7e8e4bb209706bd7c03ab10.

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,8 @@
-import AccountView from '@/views/AccountView.vue'
-import BulkAccountManagerView from '@/views/BulkAccountManagerView.vue'
-import ClientView from '@/views/ClientView.vue'
-import NewAccount from '@/views/NewAccount.vue'
-import SearchView from '@/views/SearchView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: AccountView
-    },
-    {
-      path: '/new-account',
-      name: 'new-account',
-      component: NewAccount
-    },
-    {
-      path: '/search',
-      name: 'search',
-      component: SearchView
-    },
-    {
-      path: '/client',
-      name: 'client',
-      component: ClientView
-    },
-    {
-      path: '/account-management',
-      name: 'account-management',
-      component: BulkAccountManagerView
-    },
-  ],
+  routes: [],
 })
 
 export default router
